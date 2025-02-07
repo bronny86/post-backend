50ae43be40a7923f5ead69e6b868fb55bbdc7f20
bronny86 <bronnyhamilton@gmail.com>
2025-02-08 05:40:05 +1100

    create proper folder structure

---
+++ src/routes/postRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const { getPosts } = require("../controllers/postControllers")
+
+const postRouter = express.Router()
+
+
+// GET - /posts
+postRouter.get("/", (req, res) => {
+    const posts = getPosts()
+    res.json(posts)
+})
+
+module.exports = postRouter
